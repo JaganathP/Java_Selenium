783b777dccb1942e76e7502c9acc2ae5d04a4fa6
Jaganath Padmanaban <47237994+JaganathP@users.noreply.github.com>
2020-04-10 22:08:13 +0530

    Base Project

---
+++ WalletHub_Test/target/FBHomePage-html/report.js
@@ -0,0 +1,170 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/com/wallethubtest/assignment/FBHomePage.feature");
+formatter.feature({
+  "name": "Facebook Daily Feed",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "name": "Post a Message in Facebook Daily Feed",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@facebook_page"
+    },
+    {
+      "name": "@facebook_post"
+    },
+    {
+      "name": "@sanity"
+    },
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "A user navigate to Facebook Login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "name": "user enters \"\u003cEmail\u003e\" in email textfield",
+  "keyword": "Then "
+});
+formatter.step({
+  "name": "user enters \"\u003cPassword\u003e\" in password textfield",
+  "keyword": "Then "
+});
+formatter.step({
+  "name": "user click on Login button",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "user check for the Facebook feed page",
+  "keyword": "Then "
+});
+formatter.step({
+  "name": "user enters \"\u003cMessage\u003e\" in the post and clicks Post button",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "user checks for the \"\u003cMessage\u003e\" in the daily feed",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "Email",
+        "Password",
+        "Message"
+      ]
+    },
+    {
+      "cells": [
+        "happytoday1029@gmail.com",
+        "ASDFG@12345",
+        "Hello World"
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Post a Message in Facebook Daily Feed",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@facebook_page"
+    },
+    {
+      "name": "@facebook_post"
+    },
+    {
+      "name": "@sanity"
+    },
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "A user navigate to Facebook Login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.homepage.HomePageSteps.aUserNavigatesToFacebookLoginPage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user enters \"happytoday1029@gmail.com\" in email textfield",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.homepage.HomePageSteps.UserEnterEmail(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user enters \"ASDFG@12345\" in password textfield",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.homepage.HomePageSteps.UserEnterPassword(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user click on Login button",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.homepage.HomePageSteps.UserClicksonLogin()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user check for the Facebook feed page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.homepage.HomePageSteps.UserDailyFeed()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user enters \"Hello World\" in the post and clicks Post button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.homepage.HomePageSteps.UserEnterPost(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user checks for the \"Hello World\" in the daily feed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.homepage.HomePageSteps.UserChecksPost(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
---
+++ WalletHub_Test/target/Wallethub-html/report.js
@@ -0,0 +1,184 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/com/wallethubtest/assignment/Wallethub.feature");
+formatter.feature({
+  "name": "WalletHub Review Section",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "name": "Verify the Review Section with Four Star Rating",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@walletHub"
+    },
+    {
+      "name": "@review"
+    },
+    {
+      "name": "@sanity"
+    },
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "A user navigate to WalletHub Review page",
+  "keyword": "Given "
+});
+formatter.step({
+  "name": "user hovers and clicks on four Star review",
+  "keyword": "Then "
+});
+formatter.step({
+  "name": "user lands on review page here select Health Insurance from the dropdown",
+  "keyword": "Then "
+});
+formatter.step({
+  "name": "user enter the \"\u003cReview\u003e\" in the textarea Section",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "user clicks on Submit button on the top right header",
+  "keyword": "Then "
+});
+formatter.step({
+  "name": "user lands on login page here select Login to tab",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "user enters \"\u003cEmail\u003e\" with \"\u003cPassword\u003e\" and clicks on Login Button",
+  "keyword": "Then "
+});
+formatter.step({
+  "name": "user checks for the \"\u003cReview\u003e\" in the Test Insurance Company Page",
+  "keyword": "And "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "Review",
+        "Email",
+        "Password"
+      ]
+    },
+    {
+      "cells": [
+        "Twinkle, twinkle, little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle, twinkle little star How I wonder what you are When the blazing sun is gone When he nothing shines upon Then you show…",
+        "jagan.jaganath10@gmail.com",
+        "EE!T6c9h@XPWBr@"
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Verify the Review Section with Four Star Rating",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@walletHub"
+    },
+    {
+      "name": "@review"
+    },
+    {
+      "name": "@sanity"
+    },
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "A user navigate to WalletHub Review page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.wallethubpage.WallethubPageSteps.aUserNavigatesToWallethubPage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user hovers and clicks on four Star review",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.wallethubpage.WallethubPageSteps.userClicksonFourStar()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user lands on review page here select Health Insurance from the dropdown",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.wallethubpage.WallethubPageSteps.userSelectHealthInsurance()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user enter the \"Twinkle, twinkle, little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle, twinkle little star How I wonder what you are When the blazing sun is gone When he nothing shines upon Then you show…\" in the textarea Section",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.wallethubpage.WallethubPageSteps.UserEnterReview(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user clicks on Submit button on the top right header",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.wallethubpage.WallethubPageSteps.userClicksSubmitButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user lands on login page here select Login to tab",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.wallethubpage.WallethubPageSteps.userClicksonLoginTab()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user enters \"jagan.jaganath10@gmail.com\" with \"EE!T6c9h@XPWBr@\" and clicks on Login Button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.wallethubpage.WallethubPageSteps.UserEnterEmailandPassword(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user checks for the \"Twinkle, twinkle, little star How I wonder what you are Up above the world so high Like a diamond in the sky Twinkle, twinkle little star How I wonder what you are When the blazing sun is gone When he nothing shines upon Then you show…\" in the Test Insurance Company Page",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.wallethubtest.assignment.wallethubpage.WallethubPageSteps.UsercheckstheReview(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
